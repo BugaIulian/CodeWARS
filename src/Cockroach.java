3b12637312aad5fb2fa64aec4c7f262081f77732
BugaIulian <iulian.ionut.buga@gmail.com>
2023-03-28 15:49:34 +0300

    CodeWars #5

---
+++ src/Cockroach.java
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/java
+public class Cockroach {
+    public static void main(String[] args) {
+        cockRoachSpeed(2.1);
+    }
+
+    private static int cockRoachSpeed(double v) {
+        return (int) (v * 27.7777778);
+    }
+}
