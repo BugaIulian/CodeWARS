d5d0055f4fa6334f50ce8c7352cdde8549cc474f
BugaIulian <iulian.ionut.buga@gmail.com>
2023-04-19 22:03:03 +0300

    CodeWars #9

---
+++ src/MeanSquareError.java
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/51edd51599a189fe7f000015/train/java
+public class MeanSquareError {
+
+    public static void main(String[] args) {
+        int[] a1 = {10, 10};
+        int[] a2 = {10, 10};
+        System.out.println(solution(a1, a2));
+    }
+
+    private static double solution(int[] arr1, int[] arr2) {
+        double sum = 0;
+        for (int index = 0; index < arr1.length; index++) {
+            sum += Math.pow((arr1[index] - arr2[index]) * -1, 2);
+
+        }
+        return sum / arr1.length;
+    }
+}
