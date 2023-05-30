02c57ac3e486bf6533c68e98a40b01639c77322a
Iulian <iulian.ionut.buga@gmail.com>
2023-05-30 16:39:26 +0300

    11

---
+++ src/TriangularNumberSequence.java
@@ -0,0 +1,13 @@
+//https://edabit.com/challenge/4kEHkvobTA4i6AQRE
+public class TriangularNumberSequence {
+    public static void main(String[] args) {
+
+        int number = 6;
+        int sum = 0;
+
+        for (int index = 1; index <= 6; index++) {
+            sum += index;
+            System.out.println(sum);
+        }
+    }
+}
